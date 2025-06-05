f8025efb81fc7394cfb59d0d648f392bb67a4a46
zimo493 <2081182432@qq.com>
2025-06-05 10:37:15 +0800

    feat(docs): 集成 Naive UI 并优化样式处理
    - 在 VitePress 配置中添加 SSR 相关设置，确保正确加载组件库
    - 新增自定义主题组件，实现与 Naive UI 的集成
    - 优化样式处理逻辑，提高页面加载性能

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -3,10 +3,36 @@ import { shared } from "./config/shared";
 import { zh } from "./config/zh";
 import { en } from "./config/en";
 
+const fileAndStyles: Record<string, string> = {};
+
 export default defineConfig({
   ...shared,
   locales: {
     root: { label: "简体中文", ...zh },
     en: { label: "English", ...en },
+  },
+  vite: {
+    ssr: {
+      noExternal: ["naive-ui", "date-fns", "vueuc"],
+    },
+  },
+  postRender(context) {
+    const styleRegex = /<css-render-style>((.|\s)+)<\/css-render-style>/;
+    const vitepressPathRegex = /<vitepress-path>(.+)<\/vitepress-path>/;
+    const style = styleRegex.exec(context.content)?.[1];
+    const vitepressPath = vitepressPathRegex.exec(context.content)?.[1];
+    if (vitepressPath && style) {
+      fileAndStyles[vitepressPath] = style;
+    }
+    context.content = context.content.replace(styleRegex, "");
+    context.content = context.content.replace(vitepressPathRegex, "");
+  },
+  transformHtml(code, id) {
+    const html = id.split("/").pop();
+    if (!html) return;
+    const style = fileAndStyles[`/${html}`];
+    if (style) {
+      return code.replace(/<\/head>/, style + "</head>");
+    }
   },
 });
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,72 @@
+import DefaultTheme from "vitepress/theme";
+import { defineComponent, h, inject, computed } from "vue";
+import { NConfigProvider, darkTheme } from "naive-ui";
+import { setup } from "@css-render/vue3-ssr";
+import { useRoute, useData } from "vitepress";
+
+import "./style.css";
+
+const { Layout } = DefaultTheme;
+const CssRenderStyle = defineComponent({
+  setup() {
+    const collect = inject("css-render-collect");
+    return {
+      style: collect(),
+    };
+  },
+  render() {
+    return h("css-render-style", {
+      innerHTML: this.style,
+    });
+  },
+});
+
+const VitepressPath = defineComponent({
+  setup() {
+    const route = useRoute();
+    return () => {
+      return h("vitepress-path", null, [route.path]);
+    };
+  },
+});
+
+const NaiveUIProvider = defineComponent({
+  setup() {
+    // 获取VitePress的主题状态
+    const { isDark } = useData();
+
+    // 根据VitePress的主题状态计算NaiveUI的主题
+    const theme = computed(() => (isDark.value ? darkTheme : null));
+
+    return {
+      theme,
+    };
+  },
+  render() {
+    return h(
+      NConfigProvider,
+      {
+        abstract: true,
+        inlineThemeDisabled: true,
+        theme: this.theme, // 使用计算得到的主题
+      },
+      {
+        default: () => [
+          h(Layout, null, { default: this.$slots.default?.() }),
+          import.meta.env.SSR ? [h(CssRenderStyle), h(VitepressPath)] : null,
+        ],
+      }
+    );
+  },
+});
+
+export default {
+  extends: DefaultTheme,
+  Layout: NaiveUIProvider,
+  enhanceApp: ({ app }) => {
+    if (import.meta.env.SSR) {
+      const { collect } = setup(app);
+      app.provide("css-render-collect", collect);
+    }
+  },
+};
