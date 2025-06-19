1d695fb6bcb3d0d129f431f69cbe6637aac9927e
zimo493 <2081182432@qq.com>
2025-06-19 14:04:01 +0800

    refactor(vitepress): 调整阅读增强插件的渲染顺序
    - 将阅读增强插件的渲染顺序提前至 default slot 之前
    - 优化了导航栏和屏幕内容后的菜单渲染逻辑

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -61,13 +61,13 @@ const NaiveUIProvider = defineComponent({
       { abstract: true, inlineThemeDisabled: true, theme: this.theme },
       {
         default: () => [
+          this.$slots.default?.(),
           h(Layout, null, {
             // 为较宽的屏幕的导航栏添加阅读增强菜单
             "nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
             // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
             "nav-screen-content-after": () =>
               h(NolebaseEnhancedReadabilitiesScreenMenu),
-            default: this.$slots.default?.(),
           }),
           import.meta.env.SSR ? [h(CssRenderStyle), h(VitepressPath)] : null,
         ],
