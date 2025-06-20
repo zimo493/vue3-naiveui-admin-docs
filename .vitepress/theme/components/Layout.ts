376e0f5f352f5a480d14bd361fc304ab167adbde
zimo493 <2081182432@qq.com>
2025-06-20 09:12:42 +0800

    refactor(vitepress): 重构自定义主题布局，方便后续扩展
    - 新增 Layout 组件，集中处理布局相关逻辑和样式
    - 移除 index.ts 中的冗余代码和样式导入
    - 将增强阅读能力和 git-changelog 插件的集成移至 Layout 组件

---
+++ .vitepress/theme/components/Layout.ts
@@ -0,0 +1,30 @@
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
+
+// 导入增强阅读abilities插件
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
+
+// 导入git-changelog插件的样式
+import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
+// 导入增强阅读abilities样式
+import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
+// 导入代码块图标
+import "virtual:group-icons.css";
+// 导入自定义样式
+import "../styles/index.css";
+
+export default {
+  name: "Layout",
+  render() {
+    return h(DefaultTheme.Layout, null, {
+      // 为较宽的屏幕的导航栏添加阅读增强菜单
+      "nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
+      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+      "nav-screen-content-after": () =>
+        h(NolebaseEnhancedReadabilitiesScreenMenu),
+    });
+  },
+};
